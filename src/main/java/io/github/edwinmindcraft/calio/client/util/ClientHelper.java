af2d698543afb1e42a4a8a91bc09220f25848613
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-13 15:51:53 +1100

    Calio: Fix client registry manager not clearing upon logging out.

--- src/main/java/io/github/edwinmindcraft/calio/client/util/ClientHelper.java
+++ src/main/java/io/github/edwinmindcraft/calio/client/util/ClientHelper.java
@@ -2,14 +2,18 @@ package io.github.edwinmindcraft.calio.client.util;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.RegistryAccess;
+import net.minecraftforge.server.ServerLifecycleHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class ClientHelper {
 
 	public static boolean isServerContext(@Nullable RegistryAccess access) {
 		Minecraft instance = Minecraft.getInstance();
+		//This is wrong in a data context, and annotations are busted;
+		//noinspection ConstantConditions
 		if (instance == null) return true; // Data Context
 		if (instance.getConnection() == null) return true; // Outside a world
+		if (ServerLifecycleHooks.getCurrentServer() == null) return false; // No server.
 		return access != null && access != instance.getConnection().registryAccess();
 	}
 }
